07e5f851281fb33cf1e2c789c95bd3aa247d7ae6
YSHONG\ksh40 <qkdxks0922@gmail.com>
2023-02-14 00:10:09 +0900

    2023/02/13 알고리즘 스터디

--- 2023.02.13/20230213-7.py
+++ 2023.02.13/20230213-7.py
@@ -2,16 +2,16 @@
 
 import sys
 sys.stdin = open('input.txt', 'r')
+
 T = int(input())
 for t in range(1, T + 1):
     N = int(input())
-    arr = [[1] * i for i in range(1, N + 1)]
-    for k in range(N):
-        if k >= 2:
-            for j in range(1, k-1):
-                for l in range(k-2):
-                    arr[k][j] = arr[k-1][l] + arr[k-1][l+1]
-
+    arr = [[1] * N for _ in range(1, N + 1)]
+    # if N >= 3:
+    #     for i in range(N):
+    #         if i >= 2:
+    #             arr[i]
+        
     print(f'#{t}')
     for line in arr:
         print(*line)
