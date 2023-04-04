95b01ce8280b342716da58158c287abcabcf7f90
ksykma <ksykma@naver.com>
2023-04-04 17:35:50 +0900

    2023/04/04 알고리즘 스터디

--- 2023.04.03/0403_그룹나누기.py
+++ 2023.04.03/0403_그룹나누기.py
@@ -2,3 +2,16 @@
 
 import sys
 sys.stdin = open('input.txt', 'r')
+
+T = int(input())
+for t in range(1, T + 1):
+    N, M = map(int, input().split())
+    nums = list(map(int, input().split()))
+    G = [[] for _ in range(N+1)]
+    for i in range(0, 2*M, 2):
+        u, v = nums[i], nums[i+1]
+        G[u].append(v)
+        G[v].append(u)
+    
+    visited = [0] * (N+1)
+    for i in range()
