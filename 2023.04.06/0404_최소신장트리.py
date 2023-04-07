ed2c60ba6d69f6743ff3b75e6f51386c433ee255
ksykma <ksykma@naver.com>
2023-04-07 17:54:50 +0900

    2023/04/07 알고리즘 스터디

--- 2023.04.06/0404_최소신장트리.py
+++ 2023.04.06/0404_최소신장트리.py
@@ -2,8 +2,27 @@
 
 import sys
 sys.stdin = open('input.txt', 'r')
+from heapq import heappop, heappush
 
 T = int(input())
 for t in range(1, T + 1):
     V, E = map(int, input().split())
-    
+    G = [[] for _ in range(V+1)]
+    for i in range(E):
+        u, v, w = map(int, input().split())
+        G[u].append((v, w))
+    Q = [(0, 0)]
+    D = [0xfffff] * (V+1)
+    D[0] = 0
+    # visit = [0]*(V+1)
+    while Q:
+        d, u = heappop(Q)
+        # visit[s] = 1
+        if D[u] < d:
+            continue
+        for v, w in G[u]:
+            if D[v] > D[u] + w:
+                D[v] = D[u] + w
+                heappush(Q, (D[v], v))
+    print(f'#{t}', D[V])
+            
